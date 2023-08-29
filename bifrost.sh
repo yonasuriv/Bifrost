ef6da72f4e3076b0ed08960af34fd2845e11277e
Jonathan Di Rico <diricojonathan@gmail.com>
2023-08-28 21:06:28 -0400

    Fixed arguments

--- bifrost.sh
+++ bifrost.sh
@@ -20,55 +20,25 @@ print_version() {
     echo "Bifrost Bridge Version: $version"
 }
 
-# Function to list available projects (excluding tmp)
+tmp_folder="zzz_BifrostEphemeral.tmp"
+
+tmp() {
+    rm -r "./projects/$tmp_folder"
+    mkdir -p "./projects/$tmp_folder"
+    cd "projects/$tmp_folder" #|| exit
+    cp "../../.core/create_target.sh" "../../projects/$tmp_folder/TARGET"
+}
+
 list_projects() {
     local project_index=1
     for project_dir in projects/*/; do
-        if [ -d "$project_dir" ] && [ "$project_dir" != "projects/tmp/" ]; then
+        if [ -d "$project_dir" ] && [ "$(basename "$project_dir")" != "$tmp_folder" ]; then
             project_name=$(basename "$project_dir")
             echo "  $project_index) Project $green2$project_name$end"
             project_index=$((project_index + 1))
         fi
     done
 }
-
-tmp() {
-    rm -r "./projects/tmp"
-    mkdir -p "./projects/tmp"
-    cd "projects/tmp" #|| exit
-    cp "../../.core/create_target.sh" "../../projects/tmp/TARGET"
-}
-
-folder_menu() {
-    counter=1
-
-    # Loop through each folder in the current directory
-    for project_dir in ./projects/*/; do
-        project_name=$(basename "$project_dir")  # Remove trailing slash and path
-        #echo "$counter. $project_name"  # Print the option
-        counter=$((counter + 1))  # Increment the counter
-    done
-
-    # Prompt the user for their choice
-    #printf "Select an option (1-%d): " "$((counter - 1))"
-    read choice
-
-    # Validate the user's choice
-    if [ "$choice" -ge 1 ] && [ "$choice" -lt "$counter" ]; then
-        counter=1
-        for project_dir in ./projects/*/; do
-            if [ "$counter" -eq "$choice" ]; then
-                selected_folder=$(basename "$project_dir")
-                break
-            fi
-            counter=$((counter + 1))
-        done
-        echo "You selected: $selected_folder"
-    else
-        echo "Invalid selection."
-    fi
-}
-
 
 # Function to display the project menu
 projects_menu() {
@@ -99,8 +69,8 @@ projects_menu_select() {
 
     if [ "$choice" = "t" ] || [ "$choice" = "T" ]; then
         tmp
-        echo "$yellow2     Starting a temporary project..$end"
-        sleep 2
+        echo "     $yellow2[EPHEMERAL]$end Initializing$white Temporary Project$end."
+        sleep 3
         start
     elif [ "$choice" = "n" ] || [ "$choice" = "N" ]; then
         echo -n "     Enter project name: $green" && read project_name
@@ -108,8 +78,8 @@ projects_menu_select() {
         cd "projects/$project_name" || exit
         echo "$end"
         cp "../../.core/create_target.sh" "../../projects/$project_name/TARGET"
-        echo "$green2     Project $project_name Created. Starting..$end"
-        sleep 2
+        echo "     $green2[PROJECT CREATED]$end Initializing $white$project_name$end."
+        sleep 3
         start
     elif [ "$choice" = "0" ]; then
         credits
@@ -124,7 +94,8 @@ projects_menu_select() {
                 fi
                 counter=$((counter + 1))
             done
-            echo "You selected: $selected_folder"
+            echo "     $cyan2[PROJECT FOUND]$end Initializing $white$selected_folder$end."
+            sleep 3
             cd "projects/$selected_folder"
             start
         else
@@ -132,8 +103,6 @@ projects_menu_select() {
         fi
     fi
 }
-
-
 
 # Function to handle command-line arguments
 handle_arguments() {
@@ -144,8 +113,30 @@ handle_arguments() {
                 exit 0
                 ;;
             -p)
-                clear # debug, delete after
-                projects_menu
+                shift  # Consume the -p flag
+                if [ $# -eq 0 ]; then
+                    #echo "Missing project name after -p option."
+                    #exit 1
+                    projects_menu
+                fi
+                project_name="$1"
+                
+                # Check if the folder already exists
+                if [ -d "projects/$project_name" ]; then
+                    echo
+                    echo "     $cyan2[PROJECT FOUND]$end Initializing $white$project_name$end."
+                    sleep 3
+                    cd "projects/$project_name"
+                    start
+                else
+                    mkdir -p "projects/$project_name"
+                    cd "projects/$project_name" || exit
+                    echo "$green"
+                    cp "../../.core/create_target.sh" "../../projects/$project_name/TARGET"
+                    echo "     $green2[PROJECT CREATED]$end Initializing $white$project_name$end."
+                    sleep 2
+                    start
+                fi
                 ;;
             *)
                 echo "Invalid argument: $1"
@@ -155,6 +146,7 @@ handle_arguments() {
         shift
     done
 }
+
 
 handle_arguments "$@" # Pass all command-line arguments to the handle_arguments function
 start "$@"
---
+++ projects/zzz_BifrostEphemeral.tmp/TARGET
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# IP range allowed on target. For domains please include the full domain address (http:// or https://)
+target="" # 127.0.0.1
+target_username="" # admin
+target_password="" # root
+
+# Do not modify below here
+ip="$target"
+ip_range="$target"
+target_domain="$target"
