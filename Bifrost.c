9d2cc523512f4c8849c7850b006db1a553654d23
Jonathan Di Rico <diricojonathan@gmail.com>
2023-08-29 20:04:36 -0400

    Updated Bifrost to v 1.0.3

--- Bifrost.c
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Assemble 
-. $origin/.core/assembler.sh
-. $origin/BuildBifrostBridge build
-
-# Bifrost Start
-handle_arguments "$@" # Pass all command-line arguments to the handle_arguments function
-bifrost_link "$@"
