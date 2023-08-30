4982a622d17ff157c6bdb4c9047519367ab02782
Jonathan Di Rico <diricojonathan@gmail.com>
2023-08-29 23:15:04 -0400

    Debugging

--- Bifrost.c
+++ Bifrost.c
@@ -4,4 +4,4 @@ origin=~/.local/share/Bifrost # Bifrost Path
 . $origin/.core/ASSEMBLER # Assemble 
 
 handle_arguments "$@" # Pass all command-line arguments to the handle_arguments function
-bifrost_link "$@" # Bifrost Start
+start "$@" # Bifrost Link Start
