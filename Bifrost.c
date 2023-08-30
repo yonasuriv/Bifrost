04ccb11cad8487ce4e030d7d50c4c5d3b36be428
Jonathan Di Rico <diricojonathan@gmail.com>
2023-08-29 22:17:00 -0400

    Global Changes in Script Behavior

--- Bifrost.c
+++ Bifrost.c
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 origin=~/.local/share/Bifrost # Bifrost Path
-. $origin/.core/assembler.sh # Assemble 
+. $origin/.core/ASSEMBLER # Assemble 
 
 handle_arguments "$@" # Pass all command-line arguments to the handle_arguments function
 bifrost_link "$@" # Bifrost Start
